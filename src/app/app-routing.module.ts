465adaba8869677744a27dc3f6322de8172d5938
Sp3ctraal <danielcaballeroaguilar9@gmail.com>
2025-04-18 11:27:48 +0200

    add rutas pagina no encontrada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,24 +5,26 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { CatalogComponent } from './catalog/catalog.component';
+import { CatalogoComponent } from './catalog/catalog.component';
 import { ProductComponent } from './product/product.component';
 import { PaymentComponent } from './payment/payment.component';
 import { ShippingComponent } from './shipping/shipping.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'catalog', component: CatalogComponent },
+  { path: 'catalog', component: CatalogoComponent },
   { path: 'product/:id', component: ProductComponent },
   { path: 'payment', component: PaymentComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'about-us', component: AboutusComponent }
+  { path: 'about-us', component: AboutusComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app-routing.module';
+import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  bootstrapApplication(AppComponent, {
+    providers: [
+      provideRouter(routes),
+      provideHttpClient() // Provee HttpClient para toda la app
+    ]
+  }).catch(err => console.error(err));
